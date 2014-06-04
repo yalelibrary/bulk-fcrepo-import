5d4832977a814559954160e1da58bfe44d948803
osmandin <osmandin@gmail.com>
2014-06-03 21:20:49 -0400

    Add import reference

---
+++ ladybird-webapp/src/main/java/edu/yale/library/ladybird/ImageServlet.java
@@ -0,0 +1,30 @@
+package edu.yale.library.ladybird;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class ImageServlet extends HttpServlet {
+
+    private Logger logger = LoggerFactory.getLogger(ImageServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String filename = request.getPathInfo().substring(1);
+        logger.debug("filename={}", filename);
+        File file = new File("/" + filename);
+        if (!file.exists()) {
+            logger.error("File does not exist");
+        }
+        response.setHeader("Content-Type", getServletContext().getMimeType(filename));
+        response.setHeader("Content-Length", String.valueOf(file.length()));
+        response.setHeader("Content-Disposition", "inline; filename=\"" + filename + "\"");
+        Files.copy(file.toPath(), response.getOutputStream());
+    }
+}
