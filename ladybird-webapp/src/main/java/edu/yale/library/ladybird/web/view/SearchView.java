04717a96088e777553d85ad9aea2396876a6199a
osmandin <osmandin@gmail.com>
2014-07-02 16:02:04 -0400

    Change search layout

--- ladybird-webapp/src/main/java/edu/yale/library/ladybird/web/view/SearchView.java
+++ ladybird-webapp/src/main/java/edu/yale/library/ladybird/web/view/SearchView.java
@@ -1,11 +1,12 @@
 package edu.yale.library.ladybird.web.view;
 
-import edu.yale.library.ladybird.entity.Object;
+import edu.yale.library.ladybird.entity.ObjectFile;
 import edu.yale.library.ladybird.entity.UserEvent;
 import edu.yale.library.ladybird.kernel.KernelBootstrap;
 import edu.yale.library.ladybird.kernel.events.Events;
 import edu.yale.library.ladybird.kernel.events.UserGeneratedEvent;
 import edu.yale.library.ladybird.persistence.dao.ObjectDAO;
+import edu.yale.library.ladybird.persistence.dao.ObjectFileDAO;
 import edu.yale.library.ladybird.persistence.dao.UserEventDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,13 +29,16 @@ public class SearchView extends AbstractView {
 
     private int oid;
 
-    private List<Object> itemList;
+    private List<ObjectFile> itemList = new ArrayList<>();
 
     @Inject
     ObjectDAO objectDAO;
 
     @Inject
     UserEventDAO userEventDAO;
+
+    @Inject
+    ObjectFileDAO objectFileDAO;
 
     @Inject
     AuthUtil authUtil;
@@ -52,11 +56,11 @@ public class SearchView extends AbstractView {
         this.oid = oid;
     }
 
-    public List<Object> getItemList() {
+    public List<ObjectFile> getItemList() {
         return itemList;
     }
 
-    public void setItemList(List<Object> itemList) {
+    public void setItemList(List<ObjectFile> itemList) {
         this.itemList = itemList;
     }
 
@@ -64,17 +68,31 @@ public class SearchView extends AbstractView {
     public String search() {
         try {
             //Post search event
-            final String netid = getCurrentUser();
+            final String netid = getCurrentUser(); //TODO authUtil
             postSearchEvent(netid, oid);
-
-            final Object o = objectDAO.findByOid(oid);
-            itemList = Collections.singletonList(o);
+            //final Object o = objectDAO.findByOid(oid);
+            //itemList = Collections.singletonList(o);
+            itemList = getItems(oid);
             return NavigationCase.OK.toString();
         } catch (Exception e) {
             logger.error(e.getMessage());
             itemList = new ArrayList<>();
             return NavigationCase.OK.toString();
         }
+    }
+
+    public List<ObjectFile> getItems(int oid) {
+        try {
+            //Find objects for only current project
+            //int currentProjectId = authUtil.getDefaultProjectForCurrentUser().getProjectId();
+            itemList = Collections.singletonList(objectFileDAO.findByOid(oid)); //TODO list, project
+            logger.trace("Item list size={}", itemList.size());
+
+        } catch (Exception e) {
+            logger.trace("Error finding items", e); //ignore
+            return Collections.emptyList();
+        }
+        return itemList;
     }
 
     private void postSearchEvent(final String netid, final int oid) {
@@ -96,13 +114,13 @@ public class SearchView extends AbstractView {
         });
     }
 
-     private String getCurrentUser() {
+    private String getCurrentUser() {
         return FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("netid").toString();
     }
 
     //TODO move to some other user event view?
     public List<UserEvent> getSearchEvents() {
-        final String SEARCH_EVENT = Events.USER_SEARCH.name();
+        final String SEARCH_EVENT = "user.search";
         List<UserEvent> list = new ArrayList<>();
         try {
             list = userEventDAO.findEventsByUser(SEARCH_EVENT, authUtil.getCurrentUser().getUsername());
