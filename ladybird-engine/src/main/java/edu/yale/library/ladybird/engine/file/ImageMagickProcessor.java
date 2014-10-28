43c0b4a2bfa32c7440e9a9691388c6c8a84a7c5e
osmandin <osmandin@gmail.com>
2014-10-28 15:18:26 -0400

    Re-factor IMP

--- ladybird-engine/src/main/java/edu/yale/library/ladybird/engine/file/ImageMagickProcessor.java
+++ ladybird-engine/src/main/java/edu/yale/library/ladybird/engine/file/ImageMagickProcessor.java
@@ -1,11 +1,9 @@
 package edu.yale.library.ladybird.engine.file;
 
-import com.google.common.base.Preconditions;
 import edu.yale.library.ladybird.entity.Settings;
 import edu.yale.library.ladybird.kernel.ApplicationProperties;
 import edu.yale.library.ladybird.persistence.dao.SettingsDAO;
 import edu.yale.library.ladybird.persistence.dao.hibernate.SettingsHibernateDAO;
-import org.apache.commons.io.FileUtils;
 import org.im4java.core.ConvertCmd;
 import org.im4java.core.IM4JavaException;
 import org.im4java.core.IMOperation;
@@ -19,90 +17,95 @@ public class ImageMagickProcessor implements ImageProcessor {
 
     private static Logger logger = LoggerFactory.getLogger(ImageMagickProcessor.class);
 
-    /** from .properties */
+    /** Image magick path, built only once */
+    private static String IMAGEMAGICK_PATH = getImgMagickPath();
+
+    /** from config .properties */
     private static final String IMJ_CMD_PATH = ApplicationProperties.CONFIG_STATE.IMAGE_MAGICK_PATH;
 
     /** from im4java */
     public static final String IM_4_JAVA_TOOPATH = "IM4JAVA_TOOPATH";
 
-    public void toFormat(final String src, final String dest) {
-        String sourceImage;
-        String destImage;
+    private static final int THUMBNAIL_HEIGHT = 150;
 
-        try {
-            sourceImage = Preconditions.checkNotNull(src);
-            destImage = Preconditions.checkNotNull(dest);
-        } catch (NullPointerException e) {
-            return; //ignore
+    private static final int THUMBNAIL_WIDTH = 150;
+
+    private static final double THUMBNAIL_QUALITY = 10;
+
+    public void toFormat(final String src, final String dst) {
+        if (src == null || dst == null) {
+            logger.debug("Source or destination image null");
+            return;
         }
 
-        final ConvertCmd cmd = new ConvertCmd();
-
-        String imageMagickPath = getImgMagickPath();
-
-        logger.trace("Image magick path={}", imageMagickPath);
-
-        if (!imageMagickPath.isEmpty()) {
-            cmd.setSearchPath(imageMagickPath);
-        } else {
-            logger.warn("ImageMagick program path property not found in .properties, db, or as IM4JAVA_TOOLPATH. "
-                    + "Assuming it exists on path anyway");
-        }
+        final ConvertCmd cmd = buildImageMagickCommand();
 
         try {
             logger.trace("Converting file={}", src);
-            final IMOperation op = new IMOperation();
-            op.addImage(sourceImage);
-            op.addImage(destImage);
+            IMOperation op = new IMOperation();
+            op.addImage(src);
+            op.addImage(dst);
             cmd.run(op);
         } catch (IOException | InterruptedException | IM4JavaException e) {
             throw new RuntimeException("Error processing image", e);
         }
     }
 
-    public void toThumbnailFormat(final String src, final String dest) {
-        String sourceImage;
-        String destImage;
-
-        try {
-            sourceImage = Preconditions.checkNotNull(src);
-            destImage = Preconditions.checkNotNull(dest);
-        } catch (NullPointerException e) {
-            return; //ignore
+    public void toThumbnailFormat(final String src, final String dst) {
+        if (src == null || dst == null) {
+            logger.debug("Source or destination image null");
+            return;
         }
 
-        final ConvertCmd cmd = new ConvertCmd();
-        final String imageMagickPath = getImgMagickPath();
-
-        if (!imageMagickPath.isEmpty()) {
-            cmd.setSearchPath(imageMagickPath);
-        } else {
-            logger.warn("ImageMagick program path property not found in .properties, db, or as IM4JAVA_TOOLPATH. "
-                    + "Assuming it exists on path anyway");
-        }
+        final ConvertCmd cmd = buildImageMagickCommand();
 
         try {
-            final IMOperation op2 = new IMOperation();
-            op2.addImage(sourceImage);
-            op2.thumbnail(150, 150);
-            op2.quality((double) 10);
-            op2.strip();
-            op2.addImage(destImage);
-            cmd.run(op2);
+            IMOperation op = new IMOperation();
+            op.addImage(src);
+            op.thumbnail(THUMBNAIL_HEIGHT, THUMBNAIL_WIDTH);
+            op.quality(THUMBNAIL_QUALITY);
+            op.strip();
+            op.addImage(dst);
+            cmd.run(op);
         } catch (IOException | InterruptedException | IM4JavaException e) {
             throw new RuntimeException("Error processing thumbnail", e);
         }
     }
 
+    public static String getBlankImagePath() {
+        try {
+            final SettingsDAO settingsDAO = new SettingsHibernateDAO();
+            final Settings s = settingsDAO.findByProperty(ApplicationProperties.NO_IMAGE_FOUND_PATH);
+            return s.getValue();
+        } catch (Exception e) {
+            logger.error("Error getting blank image path", e);
+            throw new NullPointerException("No image found!");
+        }
+    }
+
+    private ConvertCmd buildImageMagickCommand(){
+        final ConvertCmd cmd = new ConvertCmd();
+        final String imageMagickPath = IMAGEMAGICK_PATH;
+
+        if (!imageMagickPath.isEmpty()) {
+            cmd.setSearchPath(imageMagickPath);
+        } else {
+            logger.warn("ImageMagick path not found in .properties, db, or as IM4JAVA_TOOLPATH.");
+            logger.warn("Assuming ImageMagick exists on path anyway");
+        }
+        return cmd;
+    }
+
+
     /**
-     * Get image magick path
+     * Get image magick path or empty string
      * @return path or empty string since the field is held statically for now
      */
-    public static String getImgMagickPath() {
+    private static String getImgMagickPath() {
         try {
             final SettingsDAO settingsDAO = new SettingsHibernateDAO();
             final Settings s = settingsDAO.findByProperty(ApplicationProperties.IMAGE_MAGICK_PATH_ID);
-            String value = s.getValue();
+            final String value = s.getValue();
 
             if (!value.isEmpty()) {
                 return value;
@@ -117,21 +120,9 @@ public class ImageMagickProcessor implements ImageProcessor {
         return "";
     }
 
-    public static String getBlankImagePath() {
-        try {
-            final SettingsDAO settingsDAO = new SettingsHibernateDAO();
-            final Settings s = settingsDAO.findByProperty(ApplicationProperties.NO_IMAGE_FOUND_PATH);
-            String value = s.getValue();
-            return value;
-        } catch (Exception e) {
-            logger.error("Error getting blank image path", e);
-            throw new NullPointerException("No image found");
-        }
-    }
-
-    public static boolean pathContains(final String path) {
-        final String PROGRAM = "convert";
-        File file = new File(path + File.separator + PROGRAM);
+    private static boolean pathContains(final String path) {
+        final String TOOL = "convert";
+        File file = new File(path + File.separator + TOOL);
         return file.exists();
     }
 
